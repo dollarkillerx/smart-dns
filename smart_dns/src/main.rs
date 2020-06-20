5a7aa0ae41e5c0a7179e0b4d970ee999d00a80c8
dollarkillerx <adapawang@gmail.com>
2020-06-20 04:06:31 -0600

    upc

---
+++ smart_dns/src/main.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+use std::net::UdpSocket;
+use std::thread;
+use smart_dns::*;
+use std::sync::Arc;
+
+fn main() ->Result<(),Box<dyn Error>> {
+    // Bind an UDP socket on port 2053
+    let socket = Arc::new(UdpSocket::bind(("0.0.0.0", 2053))?);
+
+    loop {
+        let mut req_buffer = core_dns::BytePacketBuffer::new();
+        match socket.recv_from(&mut req_buffer.buf) {
+            Ok((_,addr)) => {
+                let socket_clone = socket.clone();
+                thread::spawn(move || {
+                    match core_dns::handle_query(socket_clone,addr,req_buffer) {
+                        Ok(_) => {},
+                        Err(e) => println!("Err: {}",e),
+                    }
+                });
+            },
+            Err(e) => {
+                println!("socks recv from err: {}",e);
+            }
+        }
+    }
+}
